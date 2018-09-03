7a85ad304b9ddabaac36650b081a277796d733ae
Ricardo Pereira <rpereira@mwneo.com>
2018-09-02 21:23:02 -0300

    Adding main api structure and models

---
+++ challenge/config.py
@@ -0,0 +1,11 @@
+sandbox = {
+    'api_key': 'ODk3RjhEMjFBOUY1QTpJcDE1cTZ1N1gxNUVQMjJHUzM2WG9OTHJYMkp6MHZxcQ==',
+    'url': 'http://sandboxmhubapi.epicom.com.br/v1',
+    'views': {
+        'GET_SKU_DATA': '/marketplace/produtos/{0}/skus/{1}'
+    },
+    'database': {
+        'path': '/tmp/',
+        'filename': 'sku'
+    }
+}
